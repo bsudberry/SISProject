3845ff99028d2658f09decbc7c3dfdc27a2b23f6
bsudberry <sudberry29>
2015-10-12 14:57:27 -0600

    this had main menu kinda working

--- SISProject/src/runner.java
+++ SISProject/src/runner.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 
 public class runner
 	{
-
+		static Scanner userInput = new Scanner(System.in);
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
-
+			System.out.println("What would you like to do?  (1) Add or delete student (2) Change students grade or schedule (3) Sort students");
+			int choice = userInput.nextInt();
+			if(choice == 1)
+				{
+			System.out.println("This is add or delte student");
+				}
+			if(choice == 2)
+				{
+					System.out.println("Change students grade or schedule");
+				}
+			if(choice == 3)
+				{
+					System.out.println("Sort students");
+				}
 			}
 
 	}
