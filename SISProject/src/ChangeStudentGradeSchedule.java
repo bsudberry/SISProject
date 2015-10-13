f7b51cd97cbe26d9ced8096b5f9b649b947fdd12
mary.young <mary.young@MU-C1-20.mullen.pvt.k12.co.us>
2015-10-13 14:52:31 -0600

    ChangeStudentGradeSchedule started

--- SISProject/src/ChangeStudentGradeSchedule.java
+++ SISProject/src/ChangeStudentGradeSchedule.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
 
 public class ChangeStudentGradeSchedule
 	{
-
+	static Scanner userInput = new Scanner(System.in);
+	static int choice;
+	public static void subMenuChangeGradeSchedule()
+		{
+		System.out.println("(1) Change student's grade");
+		System.out.println("(2) Change student's schedule");
+		System.out.println("(3) Return to main menu");
+		choice=userInput.nextInt();
+		if(choice==1)
+			{
+			
+			}
+		else if(choice==2)
+			{
+			
+			}
+		else if(choice==3)
+			{
+			runner.welcome();
+			}
+		}
+	public static void changeGrade()
+		{
+		
+		}
+	public static void changeSchedule()
+		{
+		
+		}
 	}
