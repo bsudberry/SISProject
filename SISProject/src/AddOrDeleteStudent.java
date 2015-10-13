8c7d3ede6a28e6cc6c9e8cfc03f5770ce87a5fec
bsudberry <sudberry29>
2015-10-13 14:49:26 -0600

    This one has the menu within the add or delete student.  By B money

--- SISProject/src/AddOrDeleteStudent.java
+++ SISProject/src/AddOrDeleteStudent.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 
 public class AddOrDeleteStudent
 	{
+		static Scanner userInput = new Scanner(System.in);
+		public static void main(String[] args)
+			{
+				askQuestion();
+			}
 
+		private static void askQuestion()
+			{
+				System.out.println("You chose to delete or add a student, please specify.");
+				System.out.println("    (1) Delete student ");
+				System.out.println("    (2) Add student ");
+				System.out.println("    (3) Return to home menu ");
+				int choice = userInput.nextInt();
+				if(choice == 1)
+					{
+						System.out.println("This is where the add student method will go");
+					}
+				if(choice == 2)
+					{
+						System.out.println("This is where the delete student method will go");
+					}
+				if(choice == 3)
+					{
+					 runner.welcome();
+					}
+			}
 	}
--- SISProject/src/runner.java
+++ SISProject/src/runner.java
@@ -6,23 +6,28 @@ public class runner
 		static Scanner userInput = new Scanner(System.in);
 		public static void main(String[] args)
 			{
-			System.out.println("What would you like to do?");
-			System.out.println("    (1) Add or delete student");
-			System.out.println("    (2) Change students grade or schedule");
-			System.out.println("    (3) Sort students");
-			int choice = userInput.nextInt();
-			if(choice == 1)
-				{
-					System.out.println("This is add or delete student");
-				}
-			if(choice == 2)
-				{
-					System.out.println("Change students grade or schedule");
-				}
-			if(choice == 3)
-				{
-					System.out.println("Sort students");
-					//Thomas was here
-				}
+				welcome();
+			
+			}
+		static void welcome()
+			{
+				System.out.println("What would you like to do?");
+				System.out.println("    (1) Add or delete student");
+				System.out.println("    (2) Change students grade or schedule");
+				System.out.println("    (3) Sort students");
+				int choice = userInput.nextInt();
+				if(choice == 1)
+					{
+						System.out.println("This is add or delete student");
+					}
+				if(choice == 2)
+					{
+						System.out.println("Change students grade or schedule");
+					}
+				if(choice == 3)
+					{
+						System.out.println("Sort students");
+					}
+				
 			}
 	}
