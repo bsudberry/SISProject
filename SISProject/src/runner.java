ac8d50eaca77c96635693947348e3fa30fd3f3ab
bsudberry <sudberry29>
2015-10-13 14:08:20 -0600

    Bronson was here.  Whats gucci

--- SISProject/src/runner.java
+++ SISProject/src/runner.java
@@ -6,7 +6,10 @@ public class runner
 		static Scanner userInput = new Scanner(System.in);
 		public static void main(String[] args)
 			{
-			System.out.println("What would you like to do?  (1) Add or delete student (2) Change students grade or schedule (3) Sort students");
+			System.out.println("What would you like to do?");
+			System.out.println("    (1) Add or delete student");
+			System.out.println("    (2) Change students grade or schedule");
+			System.out.println("    (3) Sort students");
 			int choice = userInput.nextInt();
 			if(choice == 1)
 				{
