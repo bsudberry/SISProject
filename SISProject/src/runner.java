a10b22c83e567ae225f00bc1ccd52ba5d68f85ae
Grant Fecteau <Fecteau@10.0.0.190>
2015-10-15 21:07:08 -0600

    Final Commit of Night, fixed Issues, Re-Added Stop function.

--- SISProject/src/runner.java
+++ SISProject/src/runner.java
@@ -8,25 +8,27 @@ public class runner
 		static Scanner userInput = new Scanner(System.in);
 		public static void main(String[] args)
 			{
+			try {
+				Student.fillClass();
+				} 
+			catch (IOException e) 
+			{
 				
+				e.printStackTrace();
+			}
+				while (keepGoing)
 				welcome();
 			
 			}
 		static void welcome()
 			{
-				try {
-					Student.fillClass();
-					} 
-				catch (IOException e) 
-				{
-					
-					e.printStackTrace();
-				}
+				
 				SortStudents.findGPA();
 				System.out.println("What would you like to do?");
 				System.out.println("    (1) Add or delete student");
 				System.out.println("    (2) Change students grade or schedule");
 				System.out.println("    (3) Sort students");
+				System.out.println("    (4) Stop");
 				int choice = userInput.nextInt();
 				if(choice == 1)
 					{
@@ -41,6 +43,11 @@ public class runner
 					{
 						SortStudents.askHowToSort();
 					}
+				if(choice == 4)
+				{
+					keepGoing = false;
+				}
+				
 			}
 		
 		public static void printStudents()
